ed2f7e380969d22bfed6e48c8a35c73cc62e1c17
Eithliu <iris.benoit@pix.fr>
2024-12-03 15:18:42 +0100

    day 3 part 2 🎉

--- day3/index.js
+++ day3/index.js
@@ -5,12 +5,28 @@ fs.readFile('./day3/data.txt', "utf8", (e, data) => {
     console.error(e);
     return;
   }
+  const enabled = true;
 
-  const reg = /mul\(([0-9]+,[0-9]+)\)/g;
+  const reg = /mul\(([0-9]+,[0-9]+)\)|do\(|don't\(/g;
   const arr = data.match(reg);
 
+  const goodArr = [];
+  const badArr = [];
+  for (let i = 0; i < arr.length; i++) {
+    const currentItem = arr[i];
+    const nextItem = arr[i+1];
+    if (currentItem === "don't(") {
+      while (currentItem === 'don\'t(' && arr[++i] !== 'do(')
+      {
+        badArr.push(arr[i])
+      }
+    } else {
+      goodArr.push(currentItem);
+    }
+  }
+  console.log('ici ?', goodArr, badArr);
   const digits = /\d+/g;
-  const digitsToAdd = arr.join('+').match(digits);
+  const digitsToAdd = goodArr.join('+').match(digits);
   let result = 0;
   for (let i = 0; i < digitsToAdd.length; i++) {
     result += digitsToAdd[i] * digitsToAdd[i+1];
